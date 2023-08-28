5d32ac507cbe329669c50aaf466a55a6e31342ca
mukail cakmak <tr.mukailcakmak@gmail.com>
2023-08-28 23:48:14 +0300

    [Mukail Cakmak] [Frontend] date input connected with service

--- frontend/src/app/component/home/home.component.ts
+++ frontend/src/app/component/home/home.component.ts
@@ -11,6 +11,20 @@ export class HomeComponent implements OnInit {
 
   constructor(private userService: UserService) { }
 
+  onDateChange(event: Event): any {
+    const selectedDate = (event.target as HTMLInputElement).value;
+    console.log('Selected date:', selectedDate);
+
+    this.userService.getCurrencyByDate(selectedDate.toString()+'T00:00:00.000Z').subscribe(
+      data => {
+        this.currencies = data;
+      },
+      err => {
+        this.currencies = null; // JSON.parse(err.error).message;
+      }
+    );
+  }
+
   ngOnInit(): void {
     this.userService.getCurrencyByDate('2023-08-25T00:00:00.000Z').subscribe(
       data => {
