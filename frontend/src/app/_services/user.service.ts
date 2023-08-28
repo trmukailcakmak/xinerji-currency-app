db95ec78a910827e0a6dbb8841a9daed5e9e44b6
mukail cakmak <tr.mukailcakmak@gmail.com>
2023-08-28 13:20:06 +0300

    [Mukail Cakmak] [Frontend] some area fixed

--- frontend/src/app/_services/user.service.ts
+++ frontend/src/app/_services/user.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost:8080/api/user/';
+const API_URL = 'http://localhost:8080/api/';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +13,11 @@ const API_URL = 'http://localhost:8080/api/user/';
 export class UserService {
   constructor(private http: HttpClient) { }
 
-  getPublicContent(): Observable<any> {
-    return this.http.get(API_URL + 'all', { responseType: 'text' });
+  getCurrencyByDate(date: any): Observable<any> {
+    return this.http.post(API_URL + 'currency/getCurrencyRateByDate', { date }, httpOptions);
   }
 
   getUserBoard(email: string): Observable<any> {
-    return this.http.get(API_URL + 'user/' + email, { responseType: 'text' });
+    return this.http.get(API_URL + 'user/user/' + email, { responseType: 'text' });
   }
 }
--- frontend/src/app/component/home/home.component.ts
+++ frontend/src/app/component/home/home.component.ts
@@ -7,12 +7,12 @@ import { UserService } from '../../_services/user.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  content?: string;
+  content?: any;
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    this.userService.getPublicContent().subscribe(
+    this.userService.getCurrencyByDate('2023-08-25T00:00:00.000Z').subscribe(
       data => {
         this.content = data;
       },
